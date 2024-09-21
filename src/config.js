d4dd01baa6d88ec263c9ad58ad06373b5bf27e43
EdgaR <130618005+ed-github1@users.noreply.github.com>
2024-09-20 20:41:04 -0600

    Update config.js PORT

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import { configDotenv } from 'dotenv'
 configDotenv()
 
 export const MONGODB_URI = process.env.MONGODB_URI
-export const PORT = process.env.PORT
+export const PORT = process.env.PORT || 4000
 export const SECRET = process.env.SECRET
 export const APP_HOST = process.env.APP_HOST
